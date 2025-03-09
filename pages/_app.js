0d8029bd3751e4784a0d0a3bdaab15075fb7e2e8
kevinjonathan30 <kevinjonathan@ymail.ne.jp>
2025-03-09 17:21:37 +0900

    last commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,26 @@
 import "@/styles/globals.css";
+import { useState, useEffect } from 'react';
+import AppContext from '@/context/AppContext';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [language, setLanguage] = useState('en');
+
+  useEffect(() => {
+    const updateLanguage = () => {
+      const browserLanguage = navigator.language || navigator.userLanguage;
+      setLanguage(browserLanguage.startsWith('ja') ? 'ja' : 'en');
+    };
+
+    updateLanguage();
+
+    window.addEventListener('languagechange', updateLanguage);
+
+    return () => {
+      window.removeEventListener('languagechange', updateLanguage);
+    };
+  }, []);
+
+  return <AppContext.Provider value={{ language, setLanguage }}>
+    <Component {...pageProps} />
+  </AppContext.Provider>;
 }
